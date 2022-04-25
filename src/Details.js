0a746d741d549ba02edcc9eb992ef1d3db528bc1
akashgamage <bakashgamage@gmail.com>
2022-04-25 20:49:08 +0530

    Special Case React Tools

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,12 @@
 import { Component } from "react";
 import { useParams } from "react-router-dom";
 import Carousel from "./Carousel";
+import ErrorBoundry from "./ErrorBoundry";
+import ThemeContext from "./ThemeContext";
+import Modal from './Modal';
 
 class Details extends Component {
-    state = { loading: true };
+    state = { loading: true, showModal: false };
 
     async componentDidMount() {
         const res = await fetch(
@@ -14,14 +17,15 @@ class Details extends Component {
         this.setState(Object.assign({ loading: false }, json.pets[0]))
     }
 
+    toggleModal = () => this.setState({ showModal: !this.state.showModal });
+
     render() {
         if (this.state.loading) {
             return <h2>loading ... </h2>;
         }
     
-        const { animal, breed, city, state, description, name, images } = this.state;
-        
-    
+        const { animal, breed, city, state, description, name, images, showModal } = this.state;
+
         return (
             <div className="details">
                 <Carousel images={images} />
@@ -30,8 +34,29 @@ class Details extends Component {
                     <h2>
                         {animal} - {breed} - {city}, {state}
                     </h2>
-                    <button>Adopt {name}</button>
+                    <ThemeContext.Consumer>
+                        {([theme]) => (
+                            <button 
+                            onClick={this.showModal} 
+                            style={{ backgroundColor: theme }}
+                            >
+                                Adopt {name}
+                            </button>
+                        )}
+                    </ThemeContext.Consumer>
                     <p>{description}</p>
+                    {
+                        showModal ? (
+                            <Modal toggleMdel={this.toggleModel}>
+                                <div>
+                                    <h1>Would you like to adopt {name}?</h1>
+                                    <div className="buttons">
+                                        <a href="https://bit.ly/pet-adopt">Yes</a>
+                                        <button onClick={this.toggleModal}>No</button>
+                                    </div> 
+                                </div>
+                            </Modal>
+                        ) : null}
                 </div>
             </div>
         );
@@ -40,7 +65,11 @@ class Details extends Component {
 
 const WrappedDetails = () => {
     const params = useParams();
-    return <Details params={params} />;
+    return(
+        <ErrorBoundry>
+            <Details params={params} />
+        </ErrorBoundry>
+    );
 };
 
 export default WrappedDetails;
